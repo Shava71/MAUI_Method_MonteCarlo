c4cbe72f061867f18d9cafe79b004443c78ffb3e
Shava71 <demdel2100@mail.ru>
2024-12-19 00:50:28 +0300

    Full project without bin&obj

---
+++ KR_TP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace KR_TP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
